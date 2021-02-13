f7f3ac4316a664524c38fa2b3f931b52e80c1378
LE-Smith <djsschmidt@googlemail.com>
2021-02-13 15:52:54 +0100

    add menu functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 import PadsWrapper from './containers/PadsWrapper';
 import MenuWrapper from './containers/MenuWrapper';
+
+const bankA = {
+  "Q": new Audio("./audio/korg_ddd_1/bass1.wav"),
+  "W": new Audio("./audio/korg_ddd_1/bass2.wav"),
+  "E": new Audio("./audio/korg_ddd_1/claps.wav"),
+  "A": new Audio("./audio/korg_ddd_1/clhh1.wav"),
+  "S": new Audio("./audio/korg_ddd_1/crash.wav"),
+  "D": new Audio("./audio/korg_ddd_1/snare1.wav"),
+  "Y": new Audio("./audio/korg_ddd_1/snare2.wav"),
+  "X": new Audio("./audio/korg_ddd_1/tom2.wav"),
+  "C": new Audio("./audio/korg_ddd_1/tom3.wav")
+}
+
+const bankB = {
+  "Q": new Audio("./audio/technics_pcm_dp50/BD.wav"),
+  "W": new Audio("./audio/technics_pcm_dp50/Clap.wav"),
+  "E": new Audio("./audio/technics_pcm_dp50/Conga1.wav"),
+  "A": new Audio("./audio/technics_pcm_dp50/Conga2.wav"),
+  "S": new Audio("./audio/technics_pcm_dp50/Ride.wav"),
+  "D": new Audio("./audio/technics_pcm_dp50/Snare.wav"),
+  "Y": new Audio("./audio/technics_pcm_dp50/Tom2.wav"),
+  "X": new Audio("./audio/technics_pcm_dp50/Tom3.wav"),
+  "C": new Audio("./audio/technics_pcm_dp50/Tom4.wav")
+}
+
+
 
 const AppWrapper = styled.div`
   background-color: #ddd;
@@ -28,19 +55,117 @@ const theme = createMuiTheme({
 });
 
 function App() {
+  const [powerIsOn, setPowerIsOn] = useState(false);
+  const powerIsOnRef = useRef(powerIsOn);
+  powerIsOnRef.current = powerIsOn;
+
+  const [bankBSelected, setBankBSelected] = useState(false);
+  const bankBSelectedRef = useRef(bankBSelected);
+  bankBSelectedRef.current = bankBSelected;
+
+  const [volume, setVolume] = useState(100);
+  const volumeRef = useRef(volume);
+  volumeRef.current = volume;
+
+  const [clickedKeys, setClickedKeys] = useState({});
+  const clickedKeysRef = useRef(clickedKeys);
+  clickedKeysRef.current = clickedKeys;
+
+  const playAudioForKey = key => {
+    const audio = bankBSelectedRef.current ? bankB[key] : bankA[key];
+    audio.volume = volumeRef.current/100;
+    audio.play();
+  }
+  
+  const stopAudioForKey = key => {
+    const audio = bankBSelectedRef.current ? bankB[key] : bankA[key];
+    audio.pause();
+    audio.currentTime = 0;
+  }
+ 
+  const addKeyToClickedKeys = (key, pressedBy) => {
+    if (!powerIsOnRef.current) return;
+    const newClickedKeys = { ...clickedKeysRef.current, [key]: pressedBy };
+    setClickedKeys(newClickedKeys);
+    playAudioForKey(key);
+  };
+  
+  const RemoveKeyFromClickedKeys = key => {
+    const newClickedKeys = { ...clickedKeysRef.current };
+    delete newClickedKeys[key];
+    setClickedKeys(newClickedKeys);
+    stopAudioForKey(key);
+  };
+
+
+  useEffect(() => {
+    const onKeyDownHandler = event => {
+      const key = event.key.toUpperCase();
+      if (!clickedKeysRef.current.hasOwnProperty(key)) {
+        addKeyToClickedKeys(key, 'keyboard');
+      }
+    };
+    
+    const onKeyUpHandler = event => {
+      const key = event.key.toUpperCase();
+      if (clickedKeysRef.current[key] === 'keyboard') {
+        RemoveKeyFromClickedKeys(key);
+      }
+    };
+
+    const onMouseDownHandler = event => {
+      const key = event.target.innerText.toUpperCase();
+      if (!clickedKeysRef.current.hasOwnProperty(key) && key.length === 1) {
+        addKeyToClickedKeys(key, 'mouse');
+      }
+    };
+
+    const onMouseUpOrOutHandler = event => {
+      const key = event.target.innerText ? event.target.innerText.toUpperCase() : '';
+      if (clickedKeysRef.current[key] === 'mouse'  && key.length === 1)  {
+        RemoveKeyFromClickedKeys(key);
+      }
+    };
+    
+    document.addEventListener('keydown', onKeyDownHandler);
+    document.addEventListener('keyup', onKeyUpHandler);
+    document.addEventListener('mousedown', onMouseDownHandler);
+    document.addEventListener('mouseup', onMouseUpOrOutHandler);
+    document.addEventListener('mouseout', onMouseUpOrOutHandler);
+    
+    return () => {
+      document.removeEventListener('keydown', onKeyDownHandler);
+      document.removeEventListener('keyup', onKeyUpHandler);
+      document.removeEventListener('mousedown', onMouseDownHandler);
+      document.removeEventListener('mouseup', onMouseUpOrOutHandler);
+      document.removeEventListener('mouseout', onMouseUpOrOutHandler);
+    };
+  }, []);
+
+  const onChangePowerHandler = (event) => {
+    setPowerIsOn(event.target.checked)
+  }
+
+  const onChangeBankHandler = (event) => {
+    setBankBSelected(event.target.checked)
+  }
+
+  const onChangeVolume = (event, newValue) => {
+    setVolume(newValue);
+  }
+
   return (
     <AppWrapper>
       <MuiThemeProvider theme={theme}>
-        <PadsWrapper />
+        <PadsWrapper clickedKeys={clickedKeys} powerIsOn={powerIsOn}/>
         <MenuWrapper 
-          powerIsOn={true}
-          onChangePower={() => console.log('powerChange')}
-          textToDisplay="Shit happens ..."
-          volume={80}
-          onChangeVolume={() => console.log('Volume changed')}
-          bankBSelected={true}
-          onChangeBank={() => console.log('bank changed')}
-        
+          powerIsOn={powerIsOn}
+          onChangePower={onChangePowerHandler}
+          textToDisplay="test"
+          volume={volume}
+          onChangeVolume={onChangeVolume}
+          bankBSelected={bankBSelected}
+          onChangeBank={onChangeBankHandler}
         />
       </MuiThemeProvider>
     </AppWrapper>
--- src/containers/PadsWrapper.js
+++ src/containers/PadsWrapper.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 import PadButton from '../components/PadButton';
@@ -13,6 +13,7 @@ const StyledComponent = styled.div`
   flex-wrap: wrap;
   padding: 10px;
   user-select: none;
+  opacity: ${props => props.powerIsOn ? 1.0 : 0.4};
 
   @media (max-width: 650px) {
     width: 100%;
@@ -20,103 +21,7 @@ const StyledComponent = styled.div`
   }
 `;
 
-const bankA = {
-  "Q": new Audio("./audio/korg_ddd_1/bass1.wav"),
-  "W": new Audio("./audio/korg_ddd_1/bass2.wav"),
-  "E": new Audio("./audio/korg_ddd_1/claps.wav"),
-  "A": new Audio("./audio/korg_ddd_1/clhh1.wav"),
-  "S": new Audio("./audio/korg_ddd_1/crash.wav"),
-  "D": new Audio("./audio/korg_ddd_1/snare1.wav"),
-  "Y": new Audio("./audio/korg_ddd_1/snare2.wav"),
-  "X": new Audio("./audio/korg_ddd_1/tom2.wav"),
-  "C": new Audio("./audio/korg_ddd_1/tom3.wav")
-}
-
-const bankB = {
-  "Q": new Audio("./audio/technics_pcm_dp50/BD.wav"),
-  "W": new Audio("./audio/technics_pcm_dp50/Clap.wav"),
-  "E": new Audio("./audio/technics_pcm_dp50/Conga1.wav"),
-  "A": new Audio("./audio/technics_pcm_dp50/Conga2.wav"),
-  "S": new Audio("./audio/technics_pcm_dp50/Ride.wav"),
-  "D": new Audio("./audio/technics_pcm_dp50/Snare.wav"),
-  "Y": new Audio("./audio/technics_pcm_dp50/Tom2.wav"),
-  "X": new Audio("./audio/technics_pcm_dp50/Tom3.wav"),
-  "C": new Audio("./audio/technics_pcm_dp50/Tom4.wav")
-}
-
-const playAudioForKey = key => {
-  const audio = bankB[key];
-  audio.play();
-}
-
-const stopAudioForKey = key => {
-  const audio = bankB[key];
-  audio.pause();
-  audio.currentTime = 0;
-}
-
 const Pads = props => {
-  const [clickedKeys, setClickedKeys] = useState({});
-  const clickedKeysRef = useRef(clickedKeys);
-  clickedKeysRef.current = clickedKeys;
- 
-  const addKeyToClickedKeys = (key, pressedBy) => {
-    const newClickedKeys = { ...clickedKeysRef.current, [key]: pressedBy };
-    setClickedKeys(newClickedKeys);
-    playAudioForKey(key);
-  };
-  
-  const RemoveKeyFromClickedKeys = key => {
-    const newClickedKeys = { ...clickedKeysRef.current };
-    delete newClickedKeys[key];
-    setClickedKeys(newClickedKeys);
-    stopAudioForKey(key);
-  };
-  
-  useEffect(() => {
-    const onKeyDownHandler = event => {
-      const key = event.key.toUpperCase();
-      if (!clickedKeysRef.current.hasOwnProperty(key)) {
-        addKeyToClickedKeys(key, 'keyboard');
-      }
-    };
-    
-    const onKeyUpHandler = event => {
-      const key = event.key.toUpperCase();
-      if (clickedKeysRef.current[key] === 'keyboard') {
-        RemoveKeyFromClickedKeys(key);
-      }
-    };
-
-    const onMouseDownHandler = event => {
-      const key = event.target.innerText.toUpperCase();
-      if (!clickedKeysRef.current.hasOwnProperty(key) && key.length === 1) {
-        addKeyToClickedKeys(key, 'mouse');
-      }
-    };
-
-    const onMouseUpOrOutHandler = event => {
-      const key = event.target.innerText ? event.target.innerText.toUpperCase() : '';
-      if (clickedKeysRef.current[key] === 'mouse'  && key.length === 1)  {
-        RemoveKeyFromClickedKeys(key);
-      }
-    };
-    
-    document.addEventListener('keydown', onKeyDownHandler);
-    document.addEventListener('keyup', onKeyUpHandler);
-    document.addEventListener('mousedown', onMouseDownHandler);
-    document.addEventListener('mouseup', onMouseUpOrOutHandler);
-    document.addEventListener('mouseout', onMouseUpOrOutHandler);
-    
-    return () => {
-      document.removeEventListener('keydown', onKeyDownHandler);
-      document.removeEventListener('keyup', onKeyUpHandler);
-      document.removeEventListener('mousedown', onMouseDownHandler);
-      document.removeEventListener('mouseup', onMouseUpOrOutHandler);
-      document.removeEventListener('mouseout', onMouseUpOrOutHandler);
-    };
-  }, []);
-
 
   const PadButtons = ['Q', 'W', 'E', 'A', 'S', 'D', 'Y', 'X', 'C'].map(
     (char, index) => {
@@ -124,13 +29,13 @@ const Pads = props => {
         <PadButton
           key={index}
           text={char}
-          active={clickedKeys.hasOwnProperty(char)}
+          active={props.clickedKeys.hasOwnProperty(char)}
         />
       );
     }
   );
 
-  return <StyledComponent>{PadButtons}</StyledComponent>;
+  return <StyledComponent powerIsOn={props.powerIsOn} >{PadButtons}</StyledComponent>;
 };
 
 export default Pads;
